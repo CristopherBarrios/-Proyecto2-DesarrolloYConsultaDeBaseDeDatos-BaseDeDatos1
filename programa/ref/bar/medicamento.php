790c4a006077993da652219deb4459a88f3a2cf2
RenattoGuzman <renttogs@gmail.com>
2023-04-21 15:30:53 -0600

    Alerta medicamento

--- programa/ref/bar/medicamento.php
+++ programa/ref/bar/medicamento.php
@@ -71,7 +71,7 @@
                     </li>
 
                     <li class="nav-link">
-                        <a href="reporteria.php">
+                        <a href="admin.php">
                             <i class='bx bx-pie-chart-alt icon' ></i>
                             <span class="text nav-text">Reporteria</span>
                         </a>
@@ -135,9 +135,6 @@
                                     <label for="doc_name">Establecimiento</label>
                                     <select required name="opcion" class="form-control">
                                         <option hidden value="Escoja">Selecciona una opción</option>
-								
-								
-								
 									<?php
 									require('../../php/conectar/conexion.php');
 									$res2 =  pg_query($con,"SELECT * FROM establecimiento ORDER BY estab_id ASC");
@@ -147,41 +144,9 @@
 										<option value="<?php echo $row2['estab_id']?>"> <?php echo $row2['nombre'];?></option>";
 										<?php 
 									} ?>
-
-
-
 								</select>
                                 </div>
-                            </td>
-                            <td>
-                                <div class="form-group">
-                                    <label for="unidad">Insumos</label>
-                                    <select required name="opcion" class="form-control">
-                                        <option hidden value="Escoja">Selecciona una opción</option>
-								
-								
-								
-									<?php
-									require('../../php/conectar/conexion.php');
-									$res2 =  pg_query($con,"SELECT * FROM insumos ORDER BY insumo_id ASC");
-									while($row2 = pg_fetch_array($res2))
-									{
-										?>
-										<option value="<?php echo $row2['insumo_id']?>"> <?php echo $row2['nombre'];?></option>";
-										<?php 
-									} ?>
-
-
-
-								</select>
-                                </div>
-                            </td>
-                            <td>
-                                <div class="form-group">
-                                    <label for="fecha">Caduca</label>
-                                    <input name="fecha" type="date" id="fecha" class="form-control" value="<?php echo date('Y-m-d'); ?>" required>
-                                </div>
-                            </td>
+                            </td>                            
                             <td colspan="2">
                                 <div class="form-group">
                                     <input name="submit" type="submit" class="btn btn-primary" value="Buscar">
@@ -199,22 +164,52 @@
             border-collapse: collapse;
 
         }
+
         .mi-tabla td, .mi-tabla th {
             width: 25%;
             padding: 10px;
             word-wrap: break-word;
-            /* border: 1px solid gray; */
             border-bottom: 1px solid gray;
+            text-align: center; /* centrar horizontalmente */
+            vertical-align: middle; /* centrar verticalmente */
         }
+
+        .mi-tabla th {
+            font-weight: bold;
+        }
+
+        .mi-tabla-alerta{
+            table-layout: fixed;
+            border-collapse: collapse;
+        }
+
         </style>
         <div class="separacion">
             <?php
                 require('../../php/conectar/conexion.php');
-                $sql = "SELECT * FROM inventario ORDER BY caduca ASC";
+                    
+                // Verificar si se envió el formulario
+                if (!isset($_POST['submit'])) {
+                    // Consulta SQL por defecto, sin filtrar por establecimiento
+                    $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as insumos, inventario.cantidad, inventario.caduca, CASE WHEN caduca <= NOW() THEN 'Vencido' WHEN caduca <= NOW() + INTERVAL '1 month' THEN 'Por vencer pronto' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE caduca <= NOW() OR caduca <= NOW() + INTERVAL '1 month';";
+                    } 
+                else {
+                    $establecimiento =$_POST['opcion'];
+                    // Obtener el valor seleccionado en el menú desplegable
+                    if ($establecimiento !="Escoja"){// Modificar la consulta SQL para incluir la cláusula WHERE
+                        $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as insumos, inventario.cantidad, inventario.caduca, CASE WHEN caduca <= NOW() THEN 'Vencido' WHEN caduca <= NOW() + INTERVAL '1 month' THEN 'Por vencer pronto' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE (caduca <= NOW() OR caduca <= NOW() + INTERVAL '1 month') AND establecimiento.estab_id = $establecimiento;";
+                    }
+                    else{
+                        $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as insumos, inventario.cantidad, inventario.caduca, CASE WHEN caduca <= NOW() THEN 'Vencido' WHEN caduca <= NOW() + INTERVAL '1 month' THEN 'Por vencer pronto' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE caduca <= NOW() OR caduca <= NOW() + INTERVAL '1 month';";
+                    }
+                }
+
                 $res =  pg_query($con,$sql);
+                
             ?>
             <div class="form-container2">
-                <h2 class="form-title">Resultados</h2>
+                <h2 class="form-title">Medicamento vencido/por vencer</h2>
+                
                 <table class="mi-tabla">
                     <thead>
                         <tr class="bg-primary titulo">
@@ -222,6 +217,7 @@
 				            <th>Insumos</th>
 				            <th>Cantidad</th>
 				            <th>Caduca</th>
+                            <th>Estado</th>
                         </tr>
                     </thead>
                     <?php
@@ -234,11 +230,145 @@
                             <td>".$row['insumos']."</td>
                             <td>".$row['cantidad']."</td>
                             <td>".$fecha2. "</td>
+                            <td>".$row['estado']."</td>
                         </tr>";
                     }
                     ?>
                 </table>
             </div>
+        </div>
+
+
+        <div class="separacion">
+            <?php
+                require('../../php/conectar/conexion.php');
+                // Verificar si se envió el formulario
+                if (!isset($_POST['submit'])) {
+                    // Consulta SQL por defecto, sin filtrar por establecimiento
+                    $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as  insumos , inventario.cantidad, inventario.caduca, case when cantidad <=5 then 'Últimas unidades' when (cantidad > 5 and cantidad <=15) then 'Pocas unidades' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE cantidad <=15;";
+                    } 
+                else {
+                    $establecimiento =$_POST['opcion'];
+                    // Obtener el valor seleccionado en el menú desplegable
+                    if ($establecimiento != "Escoja"){// Modificar la consulta SQL para incluir la cláusula WHERE
+                        $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as  insumos , inventario.cantidad, inventario.caduca, case when cantidad <=5 then 'Últimas unidades' when (cantidad > 5 and cantidad <=15) then 'Pocas unidades' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE (cantidad <=15) AND establecimiento.estab_id = $establecimiento;";
+                    }
+                    else{
+                        $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as  insumos , inventario.cantidad, inventario.caduca, case when cantidad <=5 then 'Últimas unidades' when (cantidad > 5 and cantidad <=15) then 'Pocas unidades' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE cantidad <=15;";
+                    }
+                }
+
+                $res =  pg_query($con,$sql);
+            ?>
+            <div class="form-container2">
+                <h2 class="form-title">Medicamento con poco stock</h2>
+                <table class="mi-tabla">
+                    <thead>
+                        <tr class="bg-primary titulo">
+                            <th>Establecimiento</th>
+				            <th>Insumos</th>
+				            <th>Cantidad</th>
+				            <th>Caduca</th>
+                            <th>Estado</th>
+                        </tr>
+                    </thead>
+                    <?php
+                    while($row = pg_fetch_array($res)){
+                        $fecha1=$row['caduca'];
+                        $fecha2=date("d-m-Y",strtotime($fecha1));
+                        echo "
+                        <tr>
+                            <td>".$row['establecimiento']."</td>
+                            <td>".$row['insumos']."</td>
+                            <td>".$row['cantidad']."</td>
+                            <td>".$fecha2. "</td>
+                            <td>".$row['estado']."</td>
+                        </tr>";
+                    }
+                    ?>
+                </table>
+            </div>
+        </div>
+
+
+
+        <div class="separacion">
+            <?php
+                require('../../php/conectar/conexion.php');
+                // Verificar si se envió el formulario
+                if (!isset($_POST['submit'])) {
+                    // Consulta SQL por defecto, sin filtrar por establecimiento
+                    $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as  insumos , inventario.cantidad, inventario.caduca, case when cantidad <=5 then 'Últimas unidades' when (cantidad > 5 and cantidad <=15) then 'Pocas unidades' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE cantidad <=15;";
+                    } 
+                else {
+                    $establecimiento =$_POST['opcion'];
+                    // Obtener el valor seleccionado en el menú desplegable
+                    if ($establecimiento != "Escoja"){// Modificar la consulta SQL para incluir la cláusula WHERE
+                        $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as  insumos , inventario.cantidad, inventario.caduca, case when cantidad <=5 then 'Últimas unidades' when (cantidad > 5 and cantidad <=15) then 'Pocas unidades' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE (cantidad <=15) AND establecimiento.estab_id = $establecimiento;";
+                    }
+                    else{
+                        $sql = "SELECT establecimiento.nombre as establecimiento, insumos.nombre as  insumos , inventario.cantidad, inventario.caduca, case when cantidad <=5 then 'Últimas unidades' when (cantidad > 5 and cantidad <=15) then 'Pocas unidades' ELSE '' END AS estado FROM inventario JOIN establecimiento ON inventario.establecimiento = establecimiento.estab_id JOIN insumos ON inventario.insumos = insumos.insumo_id WHERE cantidad <=15;";
+                    }
+                }
+
+                $res =  pg_query($con,$sql);
+            ?>
+            <div class="form-container2">
+                <h2 class="form-title">Alerta</h2>
+                            
+                <?php
+                $row2 = pg_fetch_array($res);
+                if (is_array($row2)) {
+                    // Acceder al elemento del array
+                    echo "En el establecimiento " , $row2['establecimiento'] , " se necesitan los siguientes medicamentos:" ;
+                }
+                else {
+                    echo "El establecimiento seleccionado no tiene poco stock de medicamentos";
+                }
+                                
+                ?>
+                            
+                <br><br>
+                <table class="mi-tabla-alerta">
+                    <thead>
+                        <tr class="bg-primary titulo">
+                            <th>CON URGENCIA ÚLTIMAS UNIDADES:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Reiniciar el cursor de la consulta
+                        pg_result_seek($res, 0);
+                        while($row = pg_fetch_array($res)){
+                            if ($row['estado'] == "Últimas unidades"){
+                                echo "<tr><td>".$row['insumos']."</td></tr>";
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+
+                <table class="mi-tabla-alerta">
+                    <thead>                  
+                        <tr class="bg-primary titulo">
+                            <br>
+                            <th>POCAS UNIDADES:</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Reiniciar el cursor de la consulta
+                        pg_result_seek($res, 0);
+                        while($row = pg_fetch_array($res)){
+                            if ($row['estado'] == "Pocas unidades"){
+                                echo "<tr><td>".$row['insumos']."</td></tr>";
+                            }
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     </section>
 
